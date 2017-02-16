9e8902d834fc0ffde6b9e5c1752cad8fca89944a
andela-msosan <moyosore.sosan@andela.com>
2017-02-16 22:35:44 +0100

    fix(travis): reconfigure travis file settings to use .env

--- server/models/index.js
+++ server/models/index.js
@@ -1,19 +1,17 @@
 import fs from 'fs';
 import path from 'path';
+import dotenv from 'dotenv';
 import Sequelize from 'sequelize';
 import * as dbConfig from '../../config/config.json';
+
+dotenv.config();
 
 const basename = path.basename(module.filename);
 const env = process.env.NODE_ENV || 'development';
 const config = dbConfig[env];
 const db = {};
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable]);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+const sequelize = new Sequelize(process.env[config.use_env_variable], config);
 
 fs
   .readdirSync(__dirname)
