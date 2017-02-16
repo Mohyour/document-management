5043f3ef4ea581a702bac9dbcb9cc71957334b97
andela-msosan <moyosore.sosan@andela.com>
2017-02-16 12:43:52 +0100

    feature(models): create database models and migrations
    Finishes #138141731

---
+++ server/models/role.js
@@ -0,0 +1,20 @@
+export default (sequelize, DataTypes) => {
+  const Role = sequelize.define('Role', {
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true
+    }
+  }, {
+    classMethods: {
+      associate(models) {
+        // associations can be defined here
+        Role.hasMany(models.User, {
+          foreignkey: 'id',
+          onDelete: 'CASCADE'
+        });
+      }
+    }
+  });
+  return Role;
+};
