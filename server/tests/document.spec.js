6eda0e3736a0721c4941b656120d45355943d3ec
andela-msosan <moyosore.sosan@andela.com>
2017-02-16 17:20:39 +0100

    feature(api-route): create test for document route
    WIP #139817599

---
+++ server/tests/document.spec.js
@@ -0,0 +1,189 @@
+import chai from 'chai';
+import supertest from 'supertest';
+import model from '../models';
+import app from '../../server';
+import helper from './test-helper';
+
+const request = supertest.agent(app);
+const expect = chai.expect;
+
+const adminRoleParam = helper.adminRole;
+const regularRoleParam = helper.regularRole;
+const adminUserParam = helper.adminUser;
+const regularUserParam = helper.regularUser;
+const testUserParam = helper.testUser;
+const documentOne = helper.testDocument;
+const documentTwo = helper.testDocument2;
+const documentThree = helper.testDocument3;
+
+describe('Documnet api', () => {
+  let document, adminRole, regularRole, adminUser, regularUser, testUser;
+
+  before((done) => {
+    model.Role.bulkCreate([adminRoleParam, regularRoleParam], {
+      returning: true })
+      .then((createdRoles) => {
+        adminRole = createdRoles[0];
+        regularRole = createdRoles[1];
+        adminUserParam.RoleId = adminRole.id;
+        // Two users here are assigned same RoleId to demonstrate role access
+        regularUserParam.RoleId = regularRole.id;
+        testUserParam.RoleId = regularRole.id;
+
+        request.post('/users')
+          .send(adminUserParam)
+          .end((error, response) => {
+            adminUser = response.body;
+            documentOne.UserId = adminUser.id;
+            documentOne.RoleId = adminRole.id;
+
+            request.post('/users')
+              .send(regularUserParam)
+              .end((err, res) => {
+                regularUser = res.body;
+                documentTwo.UserId = regularUser.id;
+                documentTwo.RoleId = regularRole.id;
+
+                request.post('/users')
+                  .send(testUserParam)
+                  .end((err, res) => {
+                    testUser = res.body;
+                    documentThree.UserId = testUser.id;
+                    documentThree.RoleId = regularRole.id;
+                    done();
+                  });
+              });
+          });
+      });
+  });
+
+  after(() => model.sequelize.sync({ force: true }));
+
+  describe('Post (/documents) - Create document', () => {
+    it('Should have a published date defined', (done) => {
+      request.post('/documents')
+        .send(documentOne)
+        .expect(201)
+        .end((err, res) => {
+          document = res.body;
+          expect(res.body).to.have.property('createdAt');
+          expect(res.body.createdAt).not.to.equal(null);
+          done();
+        });
+    });
+
+    it('Should have valid attributes', (done) => {
+      expect(document).to.have.property('title');
+      expect(document).to.have.property('content');
+      done();
+    });
+
+    it('Should create a document with a unique title', (done) => {
+      request.post('/documents')
+        .send(documentOne)
+        .end((err, res) => { // eslint-disable-line no-unused-vars
+          request.post('/documents')
+            .send(documentOne)
+            .end((err, res) => {
+              expect(res.body.errors[0].message).to.equal('title must be unique');
+              done();
+            });
+        });
+    });
+
+    it('Should ensure that document has an owner', (done) => {
+      request.post('/documents')
+        .send(documentTwo)
+        .expect(201)
+        .end((err, res) => {
+          document = res.body;
+          expect(res.body.UserId).to.equal(regularUser.id);
+          done();
+        });
+    });
+
+    it('Should ensure that document has a role that can access it', (done) => {
+      expect(document.RoleId).to.equal(regularRole.id);
+      done();
+    });
+
+    it('Should ensure that document cannot be created if title is lacking', (done) => {
+      documentTwo.title = null;
+      request.post('/documents')
+        .send(documentTwo)
+        .expect(201)
+        .end((err, res) => {
+          expect(res.body.errors[0].message).to.equal('title cannot be null');
+          done();
+        });
+    });
+  });
+
+  describe('Get (/document:id) - Get document', () => {
+    it('Should return all documents', (done) => {
+      request.get('/documents')
+        .expect(200).end((err, res) => {
+          expect(Array.isArray(res.body)).to.equal(true);
+          expect(res.body.length).to.be.greaterThan(0);
+          expect(res.body[0].title).to.equal(documentOne.title);
+          done();
+        });
+    });
+
+    it('Should return a document with specified id', (done) => {
+      request.get('/documents/1')
+        .expect(200).end((err, res) => {
+          expect(typeof res.body).to.equal('object');
+          expect(res.body.UserId).to.equal(adminUser.id);
+          expect(res.body.title).to.equal(documentOne.title);
+          done();
+        });
+    });
+  });
+
+  describe('PUT: (/documents/:id) - EDIT A DOCUMENT', () => {
+    it('should not perform edit if invalid id is provided', (done) => {
+      const newContent = { content: 'replace previous document' };
+      request.put('/documents/123')
+        .send(newContent)
+        .expect(404)
+        .end((error, response) => {
+          expect(response.body.message).to.equal('Document Not Found');
+          done();
+        });
+    });
+
+    it('should edit document if valid id is provided', (done) => {
+      const newContent = { content: 'replace previous document' };
+      request.put('/documents/1')
+        .send(newContent)
+        .end((error, response) => {
+          expect(response.status).to.equal(200);
+          expect(response.body.content).to.equal(newContent.content);
+          done();
+        });
+    });
+  });
+
+  describe('Delete (/document/:id) - Delete a document', () => {
+    it('Should delete a document', (done) => {
+      request.delete('/documents/1')
+        .expect(200)
+        .end((err, res) => {
+          expect(typeof res.body).to.equal('object');
+          expect(res.body.message).to.equal('Document Deleted');
+          done();
+        });
+    });
+
+    it('Should fail if document does not exist', (done) => {
+      request.delete('/documents/123')
+        .expect(404)
+        .end((err, res) => {
+          expect(typeof res.body).to.equal('object');
+          expect(res.body.message).to.equal('Document Not Found');
+          done();
+        });
+    });
+  });
+});
--- server/tests/test-helper.js
+++ server/tests/test-helper.js
@@ -31,5 +31,20 @@ export default {
     lastname: faker.name.lastName(),
     email: faker.internet.email(),
     password: faker.internet.password()
+  },
+
+  testDocument: {
+    title: faker.company.catchPhrase(),
+    content: faker.lorem.paragraph()
+  },
+
+  testDocument2: {
+    title: faker.finance.accountName(),
+    content: faker.lorem.paragraph(),
+  },
+
+  testDocument3: {
+    title: faker.commerce.department(),
+    content: faker.lorem.paragraph()
   }
 };
