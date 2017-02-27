395aef26bcd182ae6891fac18cdae5284b2b65a8
andela-msosan <moyosore.sosan@andela.com>
2017-02-27 16:35:05 +0100

    bug(heroku): fix heroku build error

--- server/seeders/20170215180330-role-seeder.js
+++ server/seeders/20170215180330-role-seeder.js
@@ -1,15 +1,15 @@
-const roleValues = require('../../data.json').role;
+// const roleValues = require('../../data.json').role;
 
 module.exports = {
   up(queryInterface, Sequelize) {
       // Add altering commands here.
       // Return a promise to correctly handle asynchronicity.
     return queryInterface.bulkInsert('Roles', [{
-      title: roleValues.roleOne || process.env.ROLEONE,
+      title: process.env.ROLEONE,
       createdAt: Sequelize.fn(('NOW')),
       updatedAt: Sequelize.fn(('NOW'))
     }, {
-      title: roleValues.roleTwo || process.env.ROLETWO,
+      title: process.env.ROLETWO,
       createdAt: Sequelize.fn(('NOW')),
       updatedAt: Sequelize.fn(('NOW'))
     }], {});
--- server/seeders/20170215205034-user-seeder.js
+++ server/seeders/20170215205034-user-seeder.js
@@ -1,19 +1,19 @@
 const bcrypt = require('bcrypt-nodejs');
-const userValues = require('../../data.json').user;
+// const userValues = require('../../data.json').user;
 
 module.exports = {
   up(queryInterface, Sequelize) {
       // Add altering commands here.
       // Return a promise to correctly handle asynchronicity.
-    const pass = userValues.password || process.env.PASSWORD;
+    const pass = process.env.PASSWORD;
     const hashedPassword = bcrypt.hashSync(pass, bcrypt.genSaltSync(8));
     return queryInterface.bulkInsert('Users', [{
-      username: userValues.username || process.env.USERNAME,
-      firstname: userValues.firstname || process.env.FIRSTNAME,
-      lastname: userValues.lastname || process.env.LASTNAME,
-      email: userValues.email || process.env.EMAIL,
+      username: process.env.USERNAME,
+      firstname: process.env.FIRSTNAME,
+      lastname: process.env.LASTNAME,
+      email: process.env.EMAIL,
       password: hashedPassword,
-      RoleId: userValues.role || process.env.ROLE,
+      RoleId: process.env.ROLE,
       createdAt: Sequelize.fn(('NOW')),
       updatedAt: Sequelize.fn(('NOW'))
     }], { individualHooks: true });
